3441f361c2cd4fa0f3725cbdc2aa426496b69323
HwaminSon <hwaminson@loplat.com>
2019-11-16 01:05:51 +0200

    expand line background

---
+++ app/src/main/java/team/gotohel/howwasyourday/LinedEditText.kt
@@ -0,0 +1,48 @@
+package team.gotohel.howwasyourday
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.util.Log
+
+import androidx.appcompat.widget.AppCompatEditText
+
+class LinedEditText// we need this constructor for LayoutInflater
+    (context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+    private val mRect: Rect = Rect()
+    private val mPaint: Paint = Paint()
+
+    init {
+        mPaint.style = Paint.Style.STROKE
+        mPaint.color = -0x7fffff01
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val count = lineCount
+        val r = mRect
+        val paint = mPaint
+
+        val lineHeight = lineHeight
+        val height = height
+
+        var baseline = 0
+        for (i in 0 until count) {
+            baseline = getLineBounds(i, r)
+
+            drawUnderLine(canvas, r, baseline, paint)
+        }
+
+        while (baseline < height) {
+            drawUnderLine(canvas, r, baseline, paint)
+            baseline += lineHeight
+        }
+
+        super.onDraw(canvas)
+    }
+
+    private fun drawUnderLine(canvas: Canvas, r: Rect, y: Int, paint: Paint) {
+        canvas.drawLine(r.left.toFloat(), (y + 1).toFloat(), r.right.toFloat(), (y + 1).toFloat(), paint)
+    }
+}
