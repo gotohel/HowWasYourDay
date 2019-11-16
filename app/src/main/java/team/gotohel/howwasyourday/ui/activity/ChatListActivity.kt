0028c4799cbbe97ed6c37f460ad70d0a09e6d159
HwaminSon <hwaminson@loplat.com>
2019-11-17 00:24:37 +0200

    대화방 목록 기능 에러 수정.

--- app/src/main/java/team/gotohel/howwasyourday/ui/activity/ChatListActivity.kt
+++ app/src/main/java/team/gotohel/howwasyourday/ui/activity/ChatListActivity.kt
@@ -1,9 +1,16 @@
 package team.gotohel.howwasyourday.ui.activity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.TypefaceSpan
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sendbird.android.GroupChannel
 import com.sendbird.android.GroupChannelListQuery
@@ -55,5 +62,29 @@ class ChatListActivity: AppCompatActivity() {
                 swipe_refresh_group_chat.isRefreshing = false
             }
         }
+    }
+
+    fun showSubMenu(view: View) {
+        val titleWrite = "Write"
+        val titleHelp = SpannableString("Help to doctor")
+        titleHelp.setSpan(ForegroundColorSpan(Color.parseColor("#E55555")), 0, titleHelp.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        titleHelp.setSpan(TypefaceSpan(resources.getFont(R.font.koho_bold)), 0, titleHelp.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        PopupMenu(this, view).apply {
+            menu.add(titleWrite)
+            menu.add(titleHelp)
+            setOnMenuItemClickListener {
+                when (it.title) {
+                    titleWrite -> {
+                        finish()
+                    }
+                    titleHelp -> {
+                        startActivity(Intent(this@ChatListActivity, DoctorDetailActivity::class.java))
+                    }
+                }
+
+                return@setOnMenuItemClickListener true
+            }
+        }.show()
     }
 }
